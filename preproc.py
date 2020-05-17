5c1c14582b2c54a87049da7dde7bd2ee59789028
Catherine Stewart <catherine.c.stwrt@gmail.com>
2020-05-17 11:02:00 -0500

    can parse the tree

---
+++ preproc.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3.7
+
+import os
+from pathlib import Path
+import pprint
+import sys
+
+START_PY = '«'
+END_PY = '»'
+START_TXT = '‹'
+END_TXT = '›'
+
+this_file = Path(os.path.realpath(__file__))
+file_dir = this_file.parent
+called_from = Path.cwd()
+in_file = Path(sys.argv[1]).resolve()
+# out_file = Path(sys.argv[2]).resolve()
+
+with in_file.open() as f:
+    raw_text = f.read()
+
+tree = []
+breadcrumbs = [tree]
+left_limit = 0
+textmode = True
+def grab_chunk(down_char, up_char):
+    global left_limit, tree, breadcrumbs
+    # find next bracket
+    right_limit = left_limit
+    while (
+            right_limit < len(raw_text)
+            and raw_text[right_limit] not in [down_char, up_char]
+            ):
+        right_limit += 1
+
+    if right_limit < len(raw_text):
+        # add current chunk to tree at working node
+        breadcrumbs[-1].append(raw_text[left_limit:right_limit])
+
+        # move in the tree
+        if raw_text[right_limit] == down_char:
+            # go deeper
+            breadcrumbs[-1].append([]) # new node
+            breadcrumbs.append(breadcrumbs[-1][-1]) # point at it
+        else:
+            # back out one level
+            breadcrumbs.pop()
+
+    # update location
+    left_limit = right_limit + 1
+
+
+while left_limit < len(raw_text):
+    if textmode:
+        grab_chunk(START_PY, END_TXT)
+    else:
+        grab_chunk(START_TXT, END_PY)
+    textmode = not textmode
+
+pprint.pprint(tree)
+
